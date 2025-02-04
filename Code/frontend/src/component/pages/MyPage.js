0b81dc1497f97907929454fd13df99ed793817e8
0and4 <iwonchu215@gmail.com>
2025-02-04 14:08:47 +0900

    MyPage > 스타일 수정

--- Code/frontend/src/component/pages/MyPage.js
+++ Code/frontend/src/component/pages/MyPage.js
@@ -3,31 +3,35 @@ import styled from "styled-components";
 import MPEdit1 from "../Popup/MPEdit1";
 import Subscribe1 from "../Popup/Subscribe1";
 import EditPL from "../Popup/EditPL";
-
-const Wrapper = styled.div`
+import { Wrapper as mpWrapper, Container as mpContainer } from "../ui/AllDiv";
+const Wrapper = styled(mpWrapper)`
   width: 100%;
-  position: relative;
-  overflow-x: hidden;
+  padding: 0;
 `;
-
-const Container = styled.div`
+const Container = styled(mpContainer)`
   width: 100%;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  div {
-    margin-left: 10px;
+  padding: 0;
+  @media (min-width: 769px) {
+    width: calc(100% - 70px);
+    margin: 0 auto;
   }
-  max-width: 100%;
+  @media (min-width: 1171px) {
+    width: calc(100% - 350px);
+    margin: 0 auto;
+  }
 `;
-
 // 프로필 섹션
 const ProfileSection = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
   padding: 20px;
+  gap: 16px;
+
   border-bottom: 1px solid #dadada;
+  @media (max-width: 768px) {
+    width: 85%;
+    margin: 0 auto;
+  }
 `;
 
 // 프로필 이미지
@@ -48,20 +52,31 @@ const ProfileImage = styled.div`
 // 프로필 정보
 const UsernameContainer = styled.div`
   display: flex;
+  justify-content: flex-start;
   align-items: center;
   gap: 10px;
+  text-align: left;
 `;
-
+const UserDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 const ProfileInfo = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   gap: 3px;
   flex-grow: 1;
+  text-align: left;
+  p {
+    margin: 0;
+  }
 `;
 
 const Username = styled.p`
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   font-weight: bold;
   margin: 0;
 `;
@@ -79,14 +94,14 @@ const EditButton = styled.button`
 `;
 
 const Email = styled.p`
-  font-size: 1.3rem;
+  font-size: 1rem;
   color: #666;
   margin: 0;
 `;
 
 // 플레이리스트 섹션
 const PlaylistSection = styled.div`
-  padding: 20px;
+  padding: 15px 20px;
 `;
 
 const PlaylistHeader = styled.div`
@@ -97,9 +112,10 @@ const PlaylistHeader = styled.div`
   padding: 0 10px;
 `;
 
-const PlaylistTitle = styled.h3`
-  font-size: 1.2rem;
+const PlaylistTitle = styled.p`
+  font-size: 1.1rem;
   text-align: left;
+  font-weight: 900;
 `;
 
 // 플레이리스트 수정 버튼
@@ -145,7 +161,8 @@ const PlaylistThumbnail = styled.div`
 `;
 
 const PlaylistName = styled.p`
-  font-size: 1.3rem;
+  font-size: 1rem;
+  font-weight: bold;
   margin: 0;
   flex-grow: 1;
   text-align: left;
@@ -160,9 +177,12 @@ const Checkbox = styled.input`
 // 구독정보 버튼
 const SubscriptionButton = styled.button`
   padding: 8px 12px;
-  font-size: 1rem;
+  height: 36px;
+  font-size: 0.9rem;
+  white-space: nowrap;
   background-color: #dadada;
   border: none;
+  border-radius: 5px;
   cursor: pointer;
   &:hover {
     background-color: rgb(146, 92, 173);
@@ -176,6 +196,7 @@ function MyPage() {
   const [isEditPLOpen, setIsEditPLOpen] = useState(false);
   const [selectedPlaylist, setSelectedPlaylist] = useState(null);
 
+  //임의의 플레이리스트 데이터, 실제 데이터 연결 시 PlaylistInfo.js로 이동하도록 설정 필요
   const playlists = [
     "내가 좋아하는 노래 💜",
     "플레이리스트 1",
@@ -197,24 +218,32 @@ function MyPage() {
       <Container>
         <ProfileSection>
           <ProfileImage>S</ProfileImage>
-          <ProfileInfo>
-            <UsernameContainer>
-              <Username>즐거운 자몽</Username>
-              <EditButton onClick={() => setIsMPEdit1Open(true)}>✏️</EditButton>
-              <MPEdit1
-                isOpen={isMPEdit1Open}
-                onClose={() => setIsMPEdit1Open(false)}
-              />
-            </UsernameContainer>
-            <Email>kim0408@gmail.com</Email>
-          </ProfileInfo>
-          <SubscriptionButton onClick={() => setIsSubscribe1Open(true)}>
-            구독정보
-          </SubscriptionButton>
-          <Subscribe1
-            isOpen={isSubscribe1Open}
-            onClose={() => setIsSubscribe1Open(false)}
-          />
+          <UserDiv>
+            <ProfileInfo>
+              <UsernameContainer>
+                <Username>
+                  <span id="nickname">즐거운 자몽</span>
+                </Username>
+                <EditButton onClick={() => setIsMPEdit1Open(true)}>
+                  ✏️
+                </EditButton>
+                <MPEdit1
+                  isOpen={isMPEdit1Open}
+                  onClose={() => setIsMPEdit1Open(false)}
+                />
+              </UsernameContainer>
+              <Email>
+                <span id="email">kim0408@gmail.com</span>
+              </Email>
+            </ProfileInfo>
+            <SubscriptionButton onClick={() => setIsSubscribe1Open(true)}>
+              구독정보
+            </SubscriptionButton>
+            <Subscribe1
+              isOpen={isSubscribe1Open}
+              onClose={() => setIsSubscribe1Open(false)}
+            />
+          </UserDiv>
         </ProfileSection>
 
         <PlaylistSection>
@@ -227,6 +256,7 @@ function MyPage() {
               >
                 수정
               </Button>
+              |
               <Button
                 onClick={() =>
                   selectedPlaylist && handleDelete(selectedPlaylist)
