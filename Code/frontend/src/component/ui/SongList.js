08f11a33f817f36ddfa82da6e3e8a95b359b5dce
jhs1255 <happyrich15142@gmail.com>
2025-02-10 22:19:39 +0900

    음악 재생을 위한 onPlay 추가

--- Code/frontend/src/component/ui/SongList.js
+++ Code/frontend/src/component/ui/SongList.js
@@ -105,7 +105,7 @@ const SongInfo = styled.div`
   min-width:100px;
 `;
 
-function SongList({ showAll, headerTitle, songs = [] }) {
+function SongList({ showAll, headerTitle, songs = [], onPlay }) {
   const navigate = useNavigate();
   const [selectedSongs, setSelectedSongs] = useState([]);
   const [allSelected, setAllSelected] = useState(false);
@@ -265,7 +265,7 @@ function SongList({ showAll, headerTitle, songs = [] }) {
         />
         </ActionColumn>
         <PlayColumn>
-          <ActionButtons songId={songId} song={song} type="play" />
+          <ActionButtons songId={songId} song={song} type="play" onPlay={onPlay} />
         </PlayColumn>
       </TableRow>
     );
