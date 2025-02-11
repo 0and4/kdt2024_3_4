3cdcc813217fbacfd94618d3a6908dbdde983649
0and4 <iwonchu215@gmail.com>
2025-02-11 11:30:25 +0900

    Playlist > 노래 저장 ì 관련 수정

--- Code/backend/berry_comment/src/main/java/com/berry_comment/repository/PlayListDetailRepository.java
+++ Code/backend/berry_comment/src/main/java/com/berry_comment/repository/PlayListDetailRepository.java
@@ -1,6 +1,8 @@
 package com.berry_comment.repository;
 
+import com.berry_comment.entity.PlayList;
 import com.berry_comment.entity.PlayListDetail;
+import com.berry_comment.entity.Song;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,5 @@ import org.springframework.data.jpa.repository.Query;
 public interface PlayListDetailRepository extends JpaRepository<PlayListDetail, Integer> {
     @Query("SELECT p.song.id from PlayListDetail p WHERE p.playList.id = :id")
     Slice<Long> findSongIdByPlayListId(Long id, Pageable pageable);
-
+    boolean existsByPlayListAndSong(PlayList playList, Song song);
 }
